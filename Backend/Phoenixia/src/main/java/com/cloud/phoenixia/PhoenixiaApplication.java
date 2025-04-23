f9d63ba577305814b717126a485da563cc713a3f
Sim-JuHeun <juheun9912@gmail.com>
2025-04-23 16:07:05 +0900

    [Backend] 초기 개발 세팅

---
+++ Backend/Phoenixia/src/main/java/com/cloud/phoenixia/PhoenixiaApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.phoenixia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoenixiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoenixiaApplication.class, args);
+    }
+
+}
